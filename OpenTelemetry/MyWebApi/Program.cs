a0d795db5910d53f4facf53d058e9441edb283d5
Douglas Canducci <douglas.canducci@gmail.com>
2025-07-17 10:46:14 -0300

    WIP

--- OpenTelemetry/MyWebApi/Program.cs
+++ OpenTelemetry/MyWebApi/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.Extensions.Compliance.Classification;
+using Microsoft.Extensions.Compliance.Redaction;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MyWebApi;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,33 +15,169 @@ builder.Services.AddHealthChecks()
             // Add a default liveness check to ensure app is responsive
             .AddCheck("self", () => HealthCheckResult.Healthy(), ["live"]);
 
+builder.Logging.ClearProviders();
+builder.Logging.EnableRedaction();
+builder.Services.AddRedaction(configure =>
+{   
+
+    configure.SetRedactor<MaskingRedactor>(
+        new DataClassificationSet(MyWebApiTaxonomy.SensitiveData)
+        );
+});
+
+builder.Services.AddMetrics();
+builder.Services.AddSingleton<WeatherApiMetrics>();
+
+string serviceName = builder.Environment.ApplicationName;
+
+ResourceBuilder resourceBuilder = ResourceBuilder.CreateDefault().AddService(serviceName, "CodeSamples", "v0.1", false, "Demo");
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(configure =>
+    {
+        configure.SetResourceBuilder(resourceBuilder);
+        configure.AddAspNetCoreInstrumentation()
+               .AddHttpClientInstrumentation()
+               .AddRuntimeInstrumentation()
+               .AddMeter(WeatherApiMetrics.MeterName);
+
+        if (!string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]))
+        {
+            configure.AddOtlpExporter();
+        }
+    })
+    .WithTracing(configure =>
+    {
+        configure.SetResourceBuilder(resourceBuilder);
+
+        configure.SetSampler(new AlwaysOnSampler());
+
+        configure.AddAspNetCoreInstrumentation()
+               .AddHttpClientInstrumentation()
+               .AddSource(builder.Environment.ApplicationName);
+
+        if (!string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]))
+        {
+            configure.AddOtlpExporter();
+        }
+    });
+
+var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
+if (useOtlpExporter)
+{
+    builder.Logging.AddOpenTelemetry(logging =>
+    {
+
+        logging.SetResourceBuilder(resourceBuilder);
+
+        logging.IncludeFormattedMessage = true;
+        logging.IncludeScopes = true;
+        logging.ParseStateValues = true;
+
+        logging.AddOtlpExporter();
+        logging.AddConsoleExporter();
+    });
+}
+else
+{
+    builder.Logging.AddJsonConsole(configure =>
+    {
+        //configure.IncludeScopes = true;
+        configure.JsonWriterOptions = new System.Text.Json.JsonWriterOptions
+        {
+            Indented = true,
+        };
+    });
+}
+
 var app = builder.Build();
 
 app.MapHealthChecks("/healthz");
 
 // Configure the HTTP request pipeline.
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var apiGroup = app.MapGroup("/weatherforecast");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+apiGroup.AddEndpointFilter<WeatherApiMetricsFilter>();
+
+apiGroup.MapGet("/", WeatherForecastHandler.GetForecasts);
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+internal record WeatherForecast(
+    DateOnly Date,
+    int TemperatureC,
+    string? Summary,
+    [SensitiveData] string? SensitiveInformation)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+internal partial class WeatherForecastHandler
+{
+    static readonly string[] summaries = [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    internal static IEnumerable<WeatherForecast> GetForecasts(ILogger<WeatherForecastHandler> logger, WeatherApiMetrics weatherApiMetrics)
+    {
+        weatherApiMetrics.RecordRequest();
+
+        var forecasts = new Bogus.Faker<WeatherForecast>()
+            .CustomInstantiator(f =>
+        {
+            var forecast = new WeatherForecast(
+                DateOnly.FromDateTime(f.Date.Future()),
+                f.Random.Int(-20, 55),
+                f.PickRandom(summaries),
+                f.Finance.CreditCardNumber()
+                );
+
+            CreatedForecast(logger, forecast.Date, forecast);
+
+            weatherApiMetrics.RecordTemperature(forecast);
+
+            return forecast;
+        })
+        .Generate(5);
+
+        return forecasts;
+    }
+
+
+    [LoggerMessage(LogLevel.Debug, Message = "Created weather forecast: {ForecastDate}")]
+    static partial void CreatedForecast(
+        ILogger logger,
+        DateOnly forecastDate,
+        [LogProperties] WeatherForecast forecast);
+}
+
+public readonly struct MyWebApiTaxonomy
+{
+    public static string TaxonomyName => typeof(MyWebApiTaxonomy).FullName!;
+
+    public static DataClassification SensitiveData => new(TaxonomyName, nameof(SensitiveData));
+}
+
+public class SensitiveDataAttribute : DataClassificationAttribute
+{
+    public SensitiveDataAttribute() : base(MyWebApiTaxonomy.SensitiveData) { }
+}
+
+public class MaskingRedactor : Redactor
+{
+
+    const string mask = "***********";
+
+    public override int GetRedactedLength(ReadOnlySpan<char> input)
+        => mask.Length;
+
+    public override int Redact(ReadOnlySpan<char> source, Span<char> destination)
+    {
+        mask.CopyTo(destination);
+
+        destination[0] = source[0];
+
+        return mask.Length;
+    }
 }
---
+++ OpenTelemetry/MyWebApi/WeatherApiMetrics.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Diagnostics.Metrics;
+using System.Diagnostics.Metrics;
+
+namespace MyWebApi;
+
+public class WeatherApiMetrics
+{
+    public const string MeterName = "MyWebApi.WeatherApi";
+    private readonly Histogram<int> weather_api_forecast_temperature;
+    private readonly Counter<int> weather_api_requests;
+
+    public WeatherApiMetrics(IMeterFactory meterFactory)
+    {
+
+        var meter = meterFactory.Create(MeterName);
+
+        weather_api_forecast_temperature = meter.CreateHistogram<int>(
+            "weather.api.forecast.temperature", "C", "Temperature (C) returned");
+        weather_api_requests = meter.CreateCounter<int>("weather.api.requests",
+            description: "Total number of requests to the Weather API");
+    }
+    public void RecordRequest()
+    {
+        weather_api_requests.Add(1);
+    }
+
+    internal void RecordTemperature(WeatherForecast forecast)
+    {
+        weather_api_forecast_temperature.Record(forecast.TemperatureC, new KeyValuePair<string, object?>("Weekday", forecast.Date.DayOfWeek.ToString()));
+    }
+}
+
+
+public class WeatherApiMetricsFilter : IEndpointFilter
+{
+    private readonly WeatherApiMetrics _metrics;
+    public WeatherApiMetricsFilter(WeatherApiMetrics metrics)
+    {
+        _metrics = metrics;
+    }
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        _metrics.RecordRequest();
+        // Call the next middleware in the pipeline
+        return await next(context);
+    }
+}
